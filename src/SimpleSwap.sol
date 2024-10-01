2474d9c485e69a7423f42a1cbb5c484b644705f2
LI-YONG-QI <a2369028@gmail.com>
2024-10-01 13:22:10 +0800

    test: swap input

--- src/SimpleSwap.sol
+++ src/SimpleSwap.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity ^0.7.0;
+pragma solidity 0.8.21;
 pragma abicoder v2;
 
 import "v3-periphery/interfaces/ISwapRouter.sol";
 import "@uniswap/v3-periphery/libraries/TransferHelper.sol";
+import "@uniswap/v3-periphery/interfaces/external/IWETH9.sol";
 
 contract SimpleSwap {
     ISwapRouter public immutable swapRouter;
--- test/SimpleSwap.t.sol
+++ test/SimpleSwap.t.sol
@@ -1,16 +1,36 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity =0.7.6;
+pragma solidity 0.8.21;
 pragma abicoder v2;
 
 import "../src/SimpleSwap.sol";
 import "v3-periphery/interfaces/ISwapRouter.sol";
+import "v3-periphery/interfaces/external/IWETH9.sol";
 import "forge-std/Test.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract SimpleSwapTest is Test {
+    address public constant WETH_ADDRESS = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address public constant DAI_ADDRESS = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    uint8 public constant DAI_DECIMALS = 18;
+    address public constant SwapRouterAddress = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
+
+    SimpleSwap simpleSwap;
+    IWETH9 WETH;
+    IERC20 DAI;
+
     function setUp() public {
-        ISwapRouter swapRouter = ISwapRouter(0x1);
-        SimpleSwap simpleSwap = new SimpleSwap(swapRouter);
+        simpleSwap = new SimpleSwap(ISwapRouter(SwapRouterAddress));
+        WETH = IWETH9(WETH_ADDRESS);
+        DAI = IERC20(DAI_ADDRESS);
     }
 
-    function testOk() public {}
+    function testSwap() public {
+        WETH.approve(address(simpleSwap), 1 ether);
+        uint256 amountIn = 0.1 ether;
+
+        simpleSwap.swapWETHForDAI(amountIn);
+
+        uint256 balance = DAI.balanceOf(address(this));
+        assertGt(balance, 0);    
+    }
 }
